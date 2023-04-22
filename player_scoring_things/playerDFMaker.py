2b63a7601c5ffc0629153512facf53103cf846ef
Vincent Guarnieri <126090788+VinGuar@users.noreply.github.com>
2023-04-22 19:15:00 +0000

    message

--- player_scoring_things/playerDFMaker.py
+++ player_scoring_things/playerDFMaker.py
@@ -26,7 +26,8 @@ def dfMaker():
 
         individualDFQB = pd.DataFrame(columns= ["Age", "PassingYds", "PassingTD", "PassingAtt", "RushingYds", "RushingTD", "RushingAtt", "Int", "Fumbles", "PPG", "ol", "rb", "wr", "qb", "te"])
         individualDFOther = pd.DataFrame(columns= ["Age", "Tgt", "Rec", "RushingYds", "RushingTD", "RushingAtt", "ReceivingYds", "ReceivingTD", "Fumbles", "PPG", "ol", "rb", "wr", "qb", "te"])       
-        
+        individualDFOther.loc[0] = [0] * len(individualDFOther.columns)
+
         year = currTeamsRoster.loc[index, 'Yrs']
         bday = currTeamsRoster.loc[index, 'BirthDate']
         pos = currTeamsRoster.loc[index, 'Pos']
@@ -49,17 +50,41 @@ def dfMaker():
             #get all player rows from past year
             playerRowRoster = pastTeamsRoster.loc[(pastTeamsRoster['Player'] == name) & (pastTeamsRoster["YearsBack"] == x) & (pastTeamsRoster["BirthDate"] == bday)].copy()
             playerRowRoster = playerRowRoster.reset_index()
+
             for ind in range(len(playerRowRoster)):
-                print(x)
                 teamCurr = playerRowRoster.loc[ind, "Team"]
                 numberCurr = playerRowRoster.loc[ind, "No."]
                 
                 if pos == "QB":
                     playerRowStats = oldQBStats.loc[(oldQBStats["Team"]== teamCurr) & (oldQBStats["Player"]==name) & (oldQBStats["No."]==numberCurr) & oldQBStats["YearsBack"]==x]
+                    playerRowStats = playerRowStats.reset_index() 
+
                 else:
                     playerRowStats = oldRushRecStats.loc[(oldRushRecStats["Team"]== teamCurr) & (oldRushRecStats["Player"]==name) & (oldRushRecStats["No."]==numberCurr) & (oldRushRecStats["YearsBack"]==x)]
-                
-                print(playerRowStats)
+                    playerRowStats = playerRowStats.reset_index() 
+
+                    #add columns to each other to get df of stats per player
+                    individualDFOther["Tgt"] = playerRowStats.loc[0, "Tgt"] + individualDFOther["Tgt"]
+                    individualDFOther["Rec"] = playerRowStats.loc[0, "Rec"] + individualDFOther["Rec"]
+                    individualDFOther["RushingYds"] = playerRowStats.loc[0, "RushYds"] + individualDFOther["RushingYds"]
+                    individualDFOther["RushingTD"] = playerRowStats.loc[0, "RushTD"] + individualDFOther["RushingTD"]            
+                    individualDFOther["RushingAtt"] = playerRowStats.loc[0, "Att"] + individualDFOther["RushingAtt"]            
+                    individualDFOther["ReceivingYds"] = playerRowStats.loc[0, "RecYds"] + individualDFOther["ReceivingYds"]            
+                    individualDFOther["ReceivingTD"] = playerRowStats.loc[0, "RecTD"] + individualDFOther["ReceivingTD"]            
+                    individualDFOther["Fumbles"] = playerRowStats.loc[0, "Fmb"] + individualDFOther["Fumbles"]            
+
+                #add other columns needed
+                individualDFOther["Age"] = age
+                print(currAVs)
+                individualDFOther["Age"] = age
+                individualDFOther["Age"] = age
+                individualDFOther["Age"] = age
+                individualDFOther["Age"] = age
+                individualDFOther["Age"] = age
+
+            
+            
+            print(individualDFOther)
 
 
             #if not playerRow.empty:
